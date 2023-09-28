f5e886af8283902c3edfb822c072d925b91ea1a6
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-28 10:05:47 +0530

    Topic-4b,4c,4d,4e

---
+++ springbootdemo/src/main/java/org/pritam/springbootdemo/controller/MyControllerOne.java
@@ -0,0 +1,15 @@
+package org.pritam.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyControllerOne {
+	@RequestMapping(value = "/",method = RequestMethod.GET)
+	@ResponseBody
+	public String welcome() {
+		return "This is spring boot demo project";
+	}
+}
