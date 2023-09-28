bb18f47574acf99eb2ee3049b765ce3c4ed28668
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-28 12:22:14 +0530

    Topic-13i,13j,13k,13l,13m

--- springbootdemo/src/main/java/org/pritam/springbootdemo/restcontroller/MyRestController.java
+++ springbootdemo/src/main/java/org/pritam/springbootdemo/restcontroller/MyRestController.java
@@ -3,46 +3,46 @@ package org.pritam.springbootdemo.restcontroller;
 import org.pritam.springbootdemo.entity.User;
 import org.pritam.springbootdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class MyRestController {
 
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/getAllUsers")
+	@GetMapping("/getAllUsers")
 	public Iterable<User> getAllUsers(){
 		return userService.getAllUsers();
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/getUser/{id}")
+	@GetMapping("/getUser/{id}")
 	public User getUser(@PathVariable("id") int id){
 		return userService.getUserById(id);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/saveUser")
+	@PostMapping("/saveUser")
 	public User saveUser(@RequestBody User user){
 		return userService.saveUser(user);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT, value = "/updateUser")
+	@PutMapping("/updateUser")
 	public User updateUser(@RequestBody User user){
 		return userService.updateUser(user);
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE, value = "/deleteAllUsers")
+	@DeleteMapping("/deleteAllUsers")
 	public void deleteAllUsers(){
 		userService.deleteAllUsers();
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE, value = "/deleteUser/{id}")
+	@DeleteMapping("/deleteUser/{id}")
 	public void deleteUser(@PathVariable("id") int id){
 		userService.deleteUserById(id);
 	}
