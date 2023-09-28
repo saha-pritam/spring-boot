8ed9f1ffae9e24e9fb3ce40c704c1de2c7e2e87a
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-28 11:02:43 +0530

    Topic-8f

--- springbootdemo/src/main/java/org/pritam/springbootdemo/entity/User.java
+++ springbootdemo/src/main/java/org/pritam/springbootdemo/entity/User.java
@@ -7,12 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name="users")
 public class User {
 	@Id
-	@Column(name="id")
+	@Column(name="userid")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	@Column(name="firstname")
--- springbootdemo/src/main/java/org/pritam/springbootdemo/service/UserService.java
+++ springbootdemo/src/main/java/org/pritam/springbootdemo/service/UserService.java
@@ -1,5 +1,7 @@
 package org.pritam.springbootdemo.service;
 
+import java.util.Optional;
+
 import org.pritam.springbootdemo.entity.User;
 import org.pritam.springbootdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,19 @@ public class UserService {
 	@Transactional
 	public User saveUser(User user) {
 		return userRepository.save(user);
+	}
+	
+	@Transactional
+	public User getUserById(int id) {
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent())
+			return user.get();
+		return null;
+	}
+	
+	@Transactional
+	public Iterable<User> getAllUsers(){
+		return userRepository.findAll();
 	}
 
 }
