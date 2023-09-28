497259be6621344d6d38426d233ecce7953fcd39
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-28 12:09:34 +0530

    Topic-13d,13e

--- springbootdemo/src/main/java/org/pritam/springbootdemo/restcontroller/MyRestController.java
+++ springbootdemo/src/main/java/org/pritam/springbootdemo/restcontroller/MyRestController.java
@@ -5,6 +5,7 @@ import org.pritam.springbootdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,5 +25,10 @@ public class MyRestController {
 	@RequestMapping(method = RequestMethod.GET, value = "/getUser/{id}")
 	public User getUser(@PathVariable("id") int id){
 		return userService.getUserById(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/saveUser")
+	public User saveUser(@RequestBody User user){
+		return userService.saveUser(user);
 	}
 }
