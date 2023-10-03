ac5e4181a8573e9248657732805a04dffa26c171
Pritam Saha <saha.pritam.work@gmail.com>
2023-10-03 11:19:24 +0530

    Topic-16d

--- thymeleafdemo/src/main/java/org/pritam/thymeleafdemo/MyController.java
+++ thymeleafdemo/src/main/java/org/pritam/thymeleafdemo/MyController.java
@@ -1,12 +1,14 @@
 package org.pritam.thymeleafdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("message", "Hi my name is pritam");
 		return "home";
 	}
 }
