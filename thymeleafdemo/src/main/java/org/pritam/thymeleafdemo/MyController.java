e05f6aaa330ede722c60d566ca005b696d942ff7
Pritam Saha <saha.pritam.work@gmail.com>
2023-10-03 11:11:35 +0530

    Topic-16c

---
+++ thymeleafdemo/src/main/java/org/pritam/thymeleafdemo/MyController.java
@@ -0,0 +1,12 @@
+package org.pritam.thymeleafdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
